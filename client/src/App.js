78249d235246f89c784fe2b27af99f0ce1c65e03
Nivedh-Biju <nivedh0203@gmail.com>
2024-05-20 16:30:18 +0530

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,37 @@
 import React, { useState } from 'react';
+import { ReactComponent as Icon } from './folder-outline.svg';
 import './App.css';
 
-
-function DescriptionModal({ show, onClose, description }) {
-  if (!show) {
-    return null;
-  }
-
-  return (
-    <div className="modal_description" onClick={onClose}>
-      <div className="modal_content_description" onClick={(e) => e.stopPropagation()}>
-        <span className="close_description" onClick={onClose}>&times;</span>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-}
 
 function App() {
   const [selectedItem, setSelectedItem] = useState(null);
   const [showModal, setShowModal] = useState(false);
 
   const arr_of_items = [
-    { name: "item one", link: "#link1#", description: "description 1 1 11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111" },
-    { name: "item two", link: "#link2#", description: "description 2 2 2" },
-    { name: "item three", link: "#link3#", description: "description 3 3 3" },
+    { name: "item one", link: "https://github.com/Nivedh-Biju/ui_for_downloads", icon:"#"},
+    { name: "item two", link: "https://github.com/Nivedh-Biju/ui_for_downloads", icon:"#"},
+    { name: "item three", link: "#link3#", icon:"#"},
   ];
 
-  const handleItemClick = (index) => {
-    setSelectedItem(index);
-    setShowModal(true);
-  };
 
-  const closeModal = () => {
-    setShowModal(false);
-    setSelectedItem(null);
-  };
+
 
   return (
     <div className="App">
       <div className="App_inner">
         {arr_of_items && (
           <ul className='display_items'>
+            <li className='heading'>
+              <span className='file_icon_heading'></span>
+              <p className='file_name_heading'>File Name</p>
+              <p className='download_button_span'>Download</p>
+            </li>
             {arr_of_items.map((application, index) => (
               <li 
                 key={index} 
                 className='individual_item'
-                onClick={() => handleItemClick(index)}
               >
+                <Icon className = "file_icon" width={24} height={24} fill="currentColor" />
                 <p className='individual_item_name'>{application.name}</p>
                 <a href={application.link} className='download_button_a'>
                   <div className='download_button'>Download</div>
@@ -56,11 +40,6 @@ function App() {
             ))}
           </ul>
         )}
-        <DescriptionModal 
-          show={showModal} 
-          onClose={closeModal} 
-          description={selectedItem !== null ? arr_of_items[selectedItem].description : ''} 
-        />
       </div>
     </div>
   );
